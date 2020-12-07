b506a42bb8bd3efa80f23c47a3b5d0abd71aba98
udcxx <me@udcxx.me>
2020-12-07 17:14:11 +0900

    a

--- prepush.js
+++ prepush.js
@@ -3,7 +3,7 @@ const branch = execSync("git rev-parse --abbrev-ref HEAD")
     .toString()
     .replace(/\r?\n/g, "");
 
-if (branch === "blog") {
+if (branch === "201207prepush-test") {
 	execSync("npm run blog");
     // execSync("node -v");
 } else {
