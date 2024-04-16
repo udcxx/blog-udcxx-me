58a72cfcdcda1fbce1b45720bf548185613cee69
udcxx <me@udcxx.me>
2024-04-16 16:08:02 +0900

    Gtagも直書きに

--- src/plugins/gtm.client.ts
+++ src/plugins/gtm.client.ts
@@ -1,11 +1,9 @@
-import secretInfos from '../../secret-infos.json';
-
 import VueGtag, { trackRouter } from 'vue-gtag-next';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueGtag, {
         property: {
-            id: secretInfos.gtag.id
+            id: 'G-RMEDS1LMCZ'
         }
     })
     trackRouter(useRouter())
